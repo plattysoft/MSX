85137e738ec0f985ac1608bc7d95a20df4226ee9
plattysoft <raul@plattysoft.com>
2020-08-16 17:58:52 +0100

    tooling update, column 10 of the mas is start / frame

--- logic_remastered/addHeader.sh
+++ logic_remastered/addHeader.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 for x in map_*; do 
   cat header.bin>src/$x.scr
-  cat $x>>src/$x.scr
+  head -c 512 $x>>src/$x.scr
 done
 cat header_full.bin>src/start.scr
-cat start_0_0>>src/start.scr
+cat map_10_0>>src/start.scr
+cat map_10_1>>src/frame.scr
--- logic_remastered/src/logic_r.bas
+++ logic_remastered/src/logic_r.bas
@@ -7,6 +7,7 @@
 70 GOTO 900
 110 DEF FN FF(X,Y)=VPEEK(&H1800+X\8+(Y\8)*32)
 120 R=0:C=0
+130 'LOAD THE GAME FRAME (room 0_0 has the complete header for screen)
 
 150 SJ=0:PS%=0:SX=46:SY=80:SV=0:TH=0:TV=0	
 
