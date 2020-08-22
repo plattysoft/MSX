41b622cc81102df500cda78f3cdc60dcf8b7776d
plattysoft <raul@plattysoft.com>
2020-08-22 20:56:50 +0100

    updated version with tiles

--- logic_remastered/createRelease.sh
+++ logic_remastered/createRelease.sh
@@ -4,6 +4,12 @@ GAME_NAME=logic_r
 rm -Rf dist
 mkdir dist
 cp src/* dist
+#openMSXbatoken.py dist/loader.bas autoexec.bas -frb
+#openMSXbatoken.py dist/$GAME_NAME.bas t_1.bas -frb
+#openMSXbatoken.py dist/logic_o.bas t_2.bas -frb
+#rm dist/loader.bas
+#mv dist/t_1.bas dist/$GAME_NAME.bas
+#mv dist/t_2.bas dist/logic_o.bas
 mv dist/loader.bas dist/autoexec.bas
 (
   echo "<command>set power off</command>";
@@ -13,5 +19,5 @@ mv dist/loader.bas dist/autoexec.bas
   echo "<command>exit</command>"
 ) | openmsx -control stdio
 
-dsk2rom -afc 1 output/$GAME_NAME.dsk output/$GAME_NAME.rom 
+dsk2rom -afc 2 $GAME_NAME.dsk $GAME_NAME.rom 
 
