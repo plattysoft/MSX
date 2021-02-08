ad9dfd3835ee16fad23d99de891c655a0a828487
plattysoft <raul@plattysoft.com>
2021-02-08 22:22:45 +0000

    removing dist, should have been in gitignore anyway

--- ilogic/createRelease.sh
+++ ilogic/createRelease.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 GAME_NAME=ilogic
 
-rm -Rf dist
-mkdir dist
-cp src/* dist
-mv dist/loader.bas dist/autoexec.bas
-
+# Find a way to automate 
 (
   echo "<command>set power off</command>";
   echo "<command>diskmanipulator create $GAME_NAME.dsk 720</command>";
   echo "<command>virtual_drive $GAME_NAME.dsk</command>";
-  echo "<command>diskmanipulator import virtual_drive dist/</command>";
+  echo "<command>diskmanipulator import virtual_drive src/</command>";
   echo "<command>exit</command>"
 ) | openmsx -control stdio
 
