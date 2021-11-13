3f6f3a0e42f39e072b56e24d1aac89b507cde3f0
plattysoft <raul@plattysoft.com>
2021-11-13 14:05:50 +0000

    laser flicking works horizontal and vertically

--- spacecat/addHeader.sh
+++ spacecat/addHeader.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-for x in level*; do 
+for x in level_*; do 
   ACTUAL_NAME=${x::7}
   echo "Converting $x to $ACTUAL_NAME"
   cat header.bin>src/$ACTUAL_NAME.scr
   cat $x>>src/$ACTUAL_NAME.scr
 done
-#rm level*
+#rm level_*
