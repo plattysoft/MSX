322b1ee88224ebe163b6904066c9c85be244ef92
plattysoft <raul@plattysoft.com>
2022-08-25 19:21:15 +0100

    endings are in place

---
+++ spacecat/parseEnding.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mv level_0_0 bad_end
+mv level_1_0 good_end
+mv level_2_0 best_end
+cp end_header.bin src/bad_end.scr
+cat bad_end >> src/bad_end.scr
+cp end_header.bin src/good_end.scr
+cat good_end >> src/good_end.scr
+cp end_header.bin src/best_end.scr
+cat best_end >> src/best_end.scr
+#rm level_*
